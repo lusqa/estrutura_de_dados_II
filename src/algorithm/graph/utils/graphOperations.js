bc428a3799746713e748bafd0d17c58e5160f821
LABINFO-FA7 - Manutencao <manutencaofa7@7setlabs.local>
2019-06-04 20:56:43 -0300

    a

--- src/algorithm/graph/utils/graphOperations.js
+++ src/algorithm/graph/utils/graphOperations.js
@@ -1,15 +1,26 @@
-export const DepthFirstSearch = (graph, vertex, discovered) => {
+export const depthFirstSearch = (graph, vertex, discovered) => {
   const incidentEdges = graph.incidentEdges(vertex)
   incidentEdges.forEach(e => {
     const opposite = e.opposite(vertex)
     if (!discovered.has(opposite)) {
       discovered.set(opposite, e)
-      DepthFirstSearch(graph, opposite, discovered)
+      depthFirstSearch(graph, opposite, discovered)
     }
   })
 }
 
-export const BreadthFirstSearch = (graph, vertex, discovered) => {
+export const depthFirstSearchComplete = graph => {
+  const forest = new Map()
+  for (let v of graph.vertices) {
+    if (!forest.has(v)) {
+      forest.set(v, null)
+      DepthFirstSearch(graph, v, forest)
+    }
+  }
+  return forest
+}
+
+export const breadthFirstSearch = (graph, vertex, discovered) => {
   const queue = [vertex]
   while (queue.length > 0) {
     const currentVertex = queue.shift()
@@ -46,4 +57,48 @@ export const toStringMap = map => {
   map.forEach((key, value, index) => {
     console.log({ key, value, index })
   })
+}
+
+// Dijkstra
+const initDijkstra = graph => {
+  const map = new Map()
+  for (let v of graph.vertices) {
+    map.set(v, {
+      predecessor: null,
+      estimate: 0,
+      isOpen: false
+    })
+  }
+  return map
+}
+
+const getVertexSmallerEstimate = vertices => {
+  vertices.forEach((value, key) => {
+    value[1]
+  })
+}
+const hasOpenVertexes = vertices => {
+  let hasOpenVertexes = false
+  vertices.forEach((vertex, key) => {
+    if (vertex.isOpen) {
+      hasOpenVertexes = true
+    }
+  })
+  return hasOpenVertexes
+}
+
+export const dijkstraShortestPaths = (graph, vertex) => {
+  let vertices = initDijkstra(graph)
+
+  console.log(vertices)
+  console.log(hasOpenVertexes(vertices))
+
+  // while (hasOpenVertexes(vertices)) {
+  //   const smallerEstimateVertex = getVertexSmallerEstimate()
+  //   smallerEstimateVertex.isOpen = false
+  //   const { outcoming } = smallerEstimateVertex
+  //   for (let i = 0; i < outcoming.length; i++) {
+  //     outcoming.relax()
+  //   }
+  // }
 }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Graph from './algorithm/graph/map/components/Graph'
 import {
-  DepthFirstSearch as DFS,
-  BreadthFirstSearch as BFS
+  depthFirstSearch as DFS,
+  breadthFirstSearch as BFS,
+  depthFirstSearchComplete as DFSComplete,
+  dijkstraShortestPaths
 } from './algorithm/graph/utils/graphOperations'
 // import Graph from './components/graphListDictonary/Graph'
 
@@ -21,6 +23,16 @@ const u = graph.addVertex('u')
 const w = graph.addVertex('w')
 const z = graph.addVertex('z')
 
+const x = graph.addVertex('x')
+const s = graph.addVertex('s')
+const r = graph.addVertex('r')
+
+const alone = graph.addVertex('alone')
+
+const c = graph.addEdge(x, s, 'c')
+const b = graph.addEdge(s, r, 'b')
+const a = graph.addEdge(r, x, 'a')
+
 // edges
 const e = graph.addEdge(v, w, 'e')
 const f = graph.addEdge(v, u, 'f')
@@ -36,6 +48,6 @@ const discovered1 = new Map()
 discovered1.set(v, null)
 BFS(graph, v, discovered1)
 
-for (let v of graph.vertices) {
-  console.log(v)
-}
+// console.log(DFSComplete(graph))
+
+dijkstraShortestPaths(graph)
